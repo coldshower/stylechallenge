3f4cd7856afaa4661207b4fa946bc37c4c79226a
John Chen <john@Johns-MBP-2.home>
2016-05-11 00:54:10 -0400

    added jquery just for presentation

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,47 @@ function toggleButton(btn) {
     $('#' + btn).addClass(toggleClass);
   }
 }
+
+// jquery just for style challenge!
+
+
+$('.add-hotel').on('click', function() {
+	setTimeout(function(){
+		$('.lorem-hotel').html('Hotel Mulberry');
+	}, 300);
+	
+});
+
+var restClicks = 0;
+$('.add-rest').on('click', function() {
+	
+	setTimeout(function() {
+		if (restClicks === 0) {
+			$('.lorem-rest').append('<li>Marc Forgione</li>');
+		} else if (restClicks === 1) {
+			$('.lorem-rest').append('<li>Tamarind</li>');
+		} else if (restClicks === 2) {
+			$('.lorem-rest').append('<li>The Capital Grille</li>');
+		}
+		restClicks += 1;
+	}, 300);
+	
+	
+});
+
+var actClicks = 0;
+$('.add-act').on('click', function() {
+	
+	setTimeout(function() {
+		if (actClicks === 0) {
+			$('.lorem-act').append('<li>Mahayana Temple Buddhist Association</li>');
+		} else if (actClicks === 1) {
+			$('.lorem-act').append('<li>Scott\'s Pizza Tours</li>');
+		} else if (actClicks === 2) {
+			$('.lorem-act').append('<li>Blue Man Group</li>');
+		}
+		actClicks += 1;
+	}, 300);
+	
+	
+});
